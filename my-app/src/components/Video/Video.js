3492e88ebbf9c603891c7284284f7137bee77cd4
Andison624 <94506603+Andison624@users.noreply.github.com>
2022-01-13 11:52:22 +0800

    Update Video.js

--- my-app/src/components/Video/Video.js
+++ my-app/src/components/Video/Video.js
@@ -3,7 +3,7 @@ import React from 'react';
 export class Video extends React.Component {
   render() {
     return (
-      <div>
+      <div>//Step5, on the render method coding a video html, and give it a src={this.props.src} , make src equal to the passed-in video file. Select App.js.
         <video controls autostart autoPlay muted src={this.props.src}/>
       </div>
     );
