66ed80cc4091a4525ecb73d4109ad4c5563b5ccc
Andison624 <94506603+Andison624@users.noreply.github.com>
2022-01-13 12:02:58 +0800

    Update Menu.js

--- my-app/src/components/Menu/Menu.js
+++ my-app/src/components/Menu/Menu.js
@@ -3,12 +3,12 @@ import React from 'react';
 export class Menu extends React.Component {
   constructor(props){
     super(props);
-    this.handleClick = this.handleClick.bind(this);
+    this.handleClick = this.handleClick.bind(this); //Step9, the handleClick pass into the render method,so i creat .bind give it a this. Now the videoplayer is run!
   }
   handleClick(e) {
   var text = e.target.value;
   this.props.chooseVideo(text);
-}
+}; //Step8,create an event listener, then pass this method into the form
   render() {
     return (
       <form onClick={this.handleClick}>
